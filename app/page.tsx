34579c8e101919d7917b37afa723ab32ddcfd576
nedal-atif <nedal_atif@hotmail.com>
2024-12-28 16:39:40 +0300

    Tailwind CSS Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl text-white font-black">Wellcome to the Next</h1>
+    <h1 className="text-3xl text-light-500 font-black">Wellcome to the Next</h1>
   );
 }
